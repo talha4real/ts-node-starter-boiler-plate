1981c00d9d805e6b42b647125be336de9d767847
talha4real <talhabutt993@gmail.com>
2022-02-28 15:11:16 +0500

    fc

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+"use strict";
+function sayMyName(name: string): void {
+    if (name === "Heisenberg") {
+      console.log("You're right 👍");
+    } else {
+      console.log("You're wrong 👎");
+    }
+  }
+   
+  sayMyName("Heisenberg");
